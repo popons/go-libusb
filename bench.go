a719834844a7c3d8db07b3442539dad0d0a29348
furukawa <neglecton@gmail.com>
2009-12-04 00:40:49 +0900

    close程度までは着ましたよ。

--- bench.go
+++ bench.go
@@ -2,12 +2,30 @@ package main
 
 import "libusb"
 import "fmt"
+import "os"
+import . "strconv"
+import "bufio"
 
 func main()
 {
     libusb.Init();
-    for _,d := range libusb.Enum()
+    for i,d := range libusb.Enum()
     {
-        fmt.Printf("BUS:%s DEVICE:%s VID:%04x PID:%04x\n",d.Bus,d.Device,d.Vid,d.Pid);
+        fmt.Printf("%3d :  BUS:%s DEVICE:%s VID:%04x PID:%04x\n",i,d.Bus,d.Device,d.Vid,d.Pid);
     }
+    fmt.Printf("どれを見るか\n");
+    fmt.Printf(":");
+
+    rd := bufio.NewReader(os.Stdin);
+    moji,_ := rd.ReadString('\n');
+    moji = moji[0:len(moji)-1];
+    fmt.Printf("sel=%s\n",moji);
+    sel,_ := Atoi(moji);
+    fmt.Printf("sel=%d\n",sel);
+
+    dev := libusb.Open(libusb.Enum()[sel]);
+
+    fmt.Printf("dev %04x %04x\n",dev.Vid,dev.Pid);
+
+    dev.Close();
 }
--- libusb.go
+++ libusb.go
@@ -4,6 +4,7 @@ package libusb
 import "C"
 
 import "fmt";
+import "io";
 
 
 func Init() (int,int)
@@ -62,3 +63,49 @@ func Enum() []Info
     }
     return infos;
 }
+
+type Device struct
+{
+    Info;
+    handle *C.usb_dev_handle;
+    //descriptor C.usb_device_descriptor;
+    io.ReadWriteCloser;
+}
+
+func Open(info Info) (*Device)
+{
+    var rdev *Device = nil;
+
+    for bus := C.usb_get_busses() ; bus != nil ; bus=bus.next
+    {
+        for dev := bus.devices ; dev!=nil ; dev = dev.next
+        {
+            if int(dev.descriptor.idVendor)  == info.Vid &&
+               int(dev.descriptor.idProduct) == info.Pid
+            {
+                h := C.usb_open(dev);
+                rdev = new(Device);
+                rdev.Info = info;
+                rdev.handle = h;
+//                rdev.descriptor = dev.descriptor;
+                return rdev;
+            }
+        }
+    }
+    return rdev;
+}
+
+func (dev *Device) Close() int
+{
+    r := int(C.usb_close(dev.handle));
+    dev.handle = nil;
+    return r;
+}
+//func (dev *Device) Product() string
+//{
+//    buf := make([]byte,1024);
+//
+//    C.usb_get_string_simple(dev.handle,dev.descriptor.iProduct,buf,len(buf));
+//
+//    return string(buf);
+//}
