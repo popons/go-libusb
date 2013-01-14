83401d6f74f2a5ed43a4824e48ebcc9326a42ff7
Steve Phillips <elimisteve@gmail.com>
2013-01-13 17:34:20 -0800

    Converted libusb.go and libusb_test.go to valid Go 1.x, then ran go fmt *.go

--- libusb_test.go
+++ libusb_test.go
@@ -2,71 +2,63 @@ package libusb
 
 import "fmt"
 import "os"
-import "testing";
+import "testing"
 
-type method func();
+type method func()
 
-func enum()
-{
+func enum() {
 
-    Init();
+	Init()
 
-    for i,info := range Enum()
-    {
-        fmt.Printf("======================================================\n");
-        fmt.Printf(" %10d : BUS:%s DEVICE:%s VID:%04x PID:%04x\n",i,info.Bus,info.Device,info.Vid,info.Pid);
-        dev := Open(info.Vid,info.Pid);
-        if dev != nil
-        {
-            fmt.Printf(" Vendor     : %s\n",dev.Vendor());
-            fmt.Printf(" Product    : %s\n",dev.Product());
-            fmt.Printf(" Last Error : %s\n",dev.LastError());
-            dev.Close();
-        }else{
-            os.Exit(1);
-        }
-    }
+	for i, info := range Enum() {
+		fmt.Printf("======================================================\n")
+		fmt.Printf(" %10d : BUS:%s DEVICE:%s VID:%04x PID:%04x\n", i, info.Bus, info.Device, info.Vid, info.Pid)
+		dev := Open(info.Vid, info.Pid)
+		if dev != nil {
+			fmt.Printf(" Vendor     : %s\n", dev.Vendor())
+			fmt.Printf(" Product    : %s\n", dev.Product())
+			fmt.Printf(" Last Error : %s\n", dev.LastError())
+			dev.Close()
+		} else {
+			os.Exit(1)
+		}
+	}
 }
 
-func conf()
-{
-    vid , pid := 0x04b4,0x8613;
+func conf() {
+	vid, pid := 0x04b4, 0x8613
 
-    Init();
+	Init()
 
-    device := Open(vid,pid);
-    println("dev=",device);
-    println("dev.bus=",device.Bus);
-    println("dev.dev=",device.Device);
-    println("dev.handle=",device.handle);
-    fmt.Printf(" Last Error : %s\n",device.LastError());
+	device := Open(vid, pid)
+	println("dev=", device)
+	println("dev.bus=", device.Bus)
+	println("dev.dev=", device.Device)
+	println("dev.handle=", device.handle)
+	fmt.Printf(" Last Error : %s\n", device.LastError())
 
-    var r int;
+	var r int
 
-    r = device.Configuration(1);
-    println("Configuration=" , r);
-    fmt.Printf(" Last Error : %s\n",device.LastError());
+	r = device.Configuration(1)
+	println("Configuration=", r)
+	fmt.Printf(" Last Error : %s\n", device.LastError())
 
-    device.Interface(0);
-    println("Interface=" , r);
-    fmt.Printf(" Last Error : %s\n",device.LastError());
-    device.Close();
+	device.Interface(0)
+	println("Interface=", r)
+	fmt.Printf(" Last Error : %s\n", device.LastError())
+	device.Close()
 }
 
+var methods = []method{
+	enum,
+	//conf,
+}
 
-var methods = []method {
-enum,
-//conf,
-};
-
-
-func TestAll(t *testing.T)
-{
-    for i,method := range methods
-    {
-        println("==============================================");
-        println("========= test ",i);
-        println("==============================================");
-        method();
-    }
+func TestAll(t *testing.T) {
+	for i, method := range methods {
+		println("==============================================")
+		println("========= test ", i)
+		println("==============================================")
+		method()
+	}
 }
